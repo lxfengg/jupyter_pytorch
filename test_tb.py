b08c2c28d25c44eeaf1d7e40572034e6ea130d4d
liuxiaofeng <liuxiaofeng@qlcd.com>
2023-10-31 17:37:02 +0800

    数据集

--- test_tb.py
+++ test_tb.py
@@ -1,6 +1,13 @@
 from torch.utils.tensorboard import SummaryWriter
+from PIL import Image
+import numpy as np
 
 writer = SummaryWriter("logs")
+img_path = "data/train/ants_image/0013035.jpg"
+img_PIL = Image.open(img_path)
+img_np = np.array(img_PIL)
+
+writer.add_image("test", img_np, 1, dataformats='HWC')
 
 for i in range(100):
     writer.add_scalar("y=x", i, i)
